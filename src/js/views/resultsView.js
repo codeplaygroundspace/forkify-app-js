a5d2ecca12df937c5f7d122ee025343aa4f2aa72
Ro <rosinaaa@proton.me>
2025-01-02 18:06:35 +0000

    display search result list

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,15 +1,42 @@
-import View from './View.js';
-import previewView from './previewView.js';
-import icons from 'url:../../img/icons.svg'; // Parcel 2
+import View from "./View.js";
+import previewView from "./previewView.js";
+import icons from "url:../../img/icons.svg"; // Parcel 2
 
-class ResultsView extends View {
-  _parentElement = document.querySelector('.results');
-  _errorMessage = 'No recipes found for your query! Please try again ;)';
-  _message = '';
+class resultsView extends View {
+  _parentElement = document.querySelector(".results");
+  _errorMessage = "No recipes found for your query! Please try again ;)";
+  _message = "";
 
   _generateMarkup() {
-    return this._data.map(result => previewView.render(result, false)).join('');
+    console.log(this._data);
+    return this._data.map(this._generateMarkupPreview).join("");
+  }
+
+  _generateMarkupPreview(result) {
+    return `<li class="preview">
+    <a class="preview__link" href="#${result.id}">
+      <figure class="preview__fig">
+        <img src="${result.image}" alt="${result.image}" />
+      </figure>
+      <div class="preview__data">
+        <h4 class="preview__title">${result.title}</h4>
+        <p class="preview__publisher">${result.publisher}</p>
+        
+      </div>
+    </a>
+  </li>`;
   }
 }
+export default new resultsView();
 
-export default new ResultsView();
+// class ResultsView extends View {
+//   _parentElement = document.querySelector('.results');
+//   _errorMessage = 'No recipes found for your query! Please try again ;)';
+//   _message = '';
+
+//   _generateMarkup() {
+//     return this._data.map(result => previewView.render(result, false)).join('');
+//   }
+// }
+
+// export default new ResultsView();
