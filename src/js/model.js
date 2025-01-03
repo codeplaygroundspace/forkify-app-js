2923512de3eeae4e5111569ca72c62f4fe2f50ae
Ro <rosinaaa@proton.me>
2025-01-03 17:11:07 +0000

    build pagination

--- src/js/model.js
+++ src/js/model.js
@@ -1,10 +1,12 @@
-import { API_URL } from "./config.js";
+import { API_URL, RES_PER_PAGE } from "./config.js";
 import { getJSON } from "./helpers.js";
 export const state = {
   recipe: {},
   search: {
     query: "",
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -48,4 +50,11 @@ export const loadSearchResults = async function (query) {
     console.error(`${err} 💥`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage; //0
+  const end = page * state.search.resultsPerPage; //9
+  return state.search.results.slice(start, end);
 };
